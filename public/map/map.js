1d230f22c8b30948457c53af44a9978a5460ea39
Cameron <camdunne@gmail.com>
2017-03-19 20:04:49 -0400

    entered and geo location options

--- public/map/map.js
+++ public/map/map.js
@@ -1,5 +1,5 @@
 angular.module('mapApp.map', [])
-.controller('mapCtrl', function($scope, $window, $http) {
+.controller('mapCtrl', function($rootScope, $scope, $window, $http) {
 
   // INITIALIZE GOOGLE MAP
 
@@ -35,7 +35,18 @@ angular.module('mapApp.map', [])
   function fsSearch(position, map) {
     $scope.fsState = 'loading';
 
-    $http.get("https://api.foursquare.com/v2/venues/explore/?ll=" + position.lat + "," + position.lng + "&limit=5&radius=1600&section=arts&openNow=1&sortByDistance=1" + "&client_id=" + window.CLIENT_ID + "&client_secret=" + window.CLIENT_SECRET + "&v=20170319&m=foursquare")
+    // sets location based on entered location or Geolocation
+    var latitude;
+    var longitude;
+    if($rootScope.address === undefined){
+      latitude = position.lat;
+      longitude = position.lng;
+    } else {
+      latitude = $rootScope.lat;
+      longitude = $rootScope.lng;
+    }
+
+    $http.get("https://api.foursquare.com/v2/venues/explore/?ll=" + latitude + "," + longitude + "&limit=5&radius=1600&section=arts&openNow=1&sortByDistance=1" + "&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20170319&m=foursquare")
       .then(function(result, status) {
         var fsPlaces = result.data.response.groups[0].items;
         var fsPlacesLatLng = [];
