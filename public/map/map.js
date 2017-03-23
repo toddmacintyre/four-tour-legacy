0dfe355616a096c5f22fc89c64c99b5ae757c677
iamongit <ayousuf2@binghamton.edu>
2017-03-23 19:36:47 -0400

    feat(maps.js css): Added markers and infowindow
    Added markers, markers with images, more info, some styling

--- public/map/map.js
+++ public/map/map.js
@@ -63,22 +63,49 @@ angular.module('mapApp.map', [])
     var data = {"latitude": latitude, "longitude": longitude}
     $http.post('/api/foursquare', data)
       .then(function(result, status) {
+        // console.log(result, "RESULT from FOURSQUARE")
         var fsPlaces = result.data.response.groups[0].items;
         var fsPlacesLatLng = [];
+
+        // fsPlaces.forEach(function(place){
+        //   var addressString = `${place.venue.name}, ${place.venue.location.address}, ${place.venue.location.city}, ${place.venue.location.state}, ${place.venue.location.cc}`;
+        //   var request = {
+        //     location: map.getCenter(),
+        //     radius: '500',
+        //     query: addressString
+        //   };
+        //   var service2 = new google.maps.places.PlacesService(map);
+        //   service2.textSearch(request, callback);
+        //   function callback(result, status){
+        //     console.log(result[0].place_id, "***********");
+        //     fsPlacesLatLng.push(
+        //     {
+        //       location:{placeId:result[0].place_id},//addressString,
+        //       stopover: true
+        //     }
+        //   );
+        //   }
+        // })
+
+
         fsPlaces.forEach(function(place) {
+          var addressString = `${place.venue.name}, ${place.venue.location.address}, ${place.venue.location.city}, ${place.venue.location.state}, ${place.venue.location.cc}`;
+          console.log(addressString);
           fsPlacesLatLng.push(
             {
-              location: {
-                lat: place.venue.location.lat,
-                lng: place.venue.location.lng
-              },
+              location:addressString,
               stopover: true
             }
           );
-        });
+        }); 
+
         $scope.fsState = 'loaded';
+        console.log(fsPlacesLatLng, "waypointsssss");
         //the start and end is based on position which is the current location position not entered
-        drawTour(position, map, fsPlacesLatLng);
+        // setTimeout(function(){
+          drawTour(position, map, fsPlacesLatLng);
+        // }, 6000)
+        // drawTour(position, map, fsPlacesLatLng);
       }, function(data, status) {
         $scope.fsState = 'noResult';
       });
@@ -88,8 +115,18 @@ angular.module('mapApp.map', [])
 
   function drawTour(position, map, fsWaypoints) {
 
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer(
+      {
+        // suppressMarkers: true,
+        // suppressInfoWindows: true, 
+        polylineOptions: { strokeColor: "green" }
+      });
+
+    
+    // var stepDisplay = new google.maps.InfoWindow;
+    // var markerArray = [];
+
 
     // var infoWindow = new google.maps.InfoWindow({map: map});
     // infoWindow.setPosition(position);
@@ -105,17 +142,115 @@ angular.module('mapApp.map', [])
     var request = {
       origin: position,
       destination: position,
-      travelMode: 'WALKING',
+      travelMode: google.maps.TravelMode.WALKING, //'WALKING',
       waypoints: fsWaypoints,
       optimizeWaypoints: true
     };
 
+    var placeIds = []
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
     directionsService.route(request, function (response, status) {
+      // console.log(response, "RESPONse from google ROUTES")
+
+      response.geocoded_waypoints.forEach(function(elle){
+      // placeIds.push(elle.place_id);
+      // console.log('elleplc IDIDIID',elle['place_id'])
+      service.getDetails({placeId:elle['place_id']}, function(place, status){
+        // console.log(place, "plac!!!!!!!!!!!!!!!!!!!!!!")
+        var imageUrls = [];
+        if(place.photos){
+          place.photos.forEach(function(object){
+            var urlPic = object.getUrl({'maxWidth': 200, 'maxHeight': 200})
+            imageUrls.push(urlPic);
+          })
+        }
+        var icon = "https://s15.postimg.org/rlm6kl1rf/iconiccopy.png";
+        var marker = new google.maps.Marker({
+          map: map,
+          icon:"/img/icon.PNG",
+          position: place.geometry.location
+        });
+
+        // markerArray.push(marker);
+        console.log(imageUrls, "URLSSSSS");
+        var template =  "<div class='infoWin1' style='width:250px; height:250px;'><strong style='font-size: 20px;'>" + place.name + "</strong>" +
+            "<br>" + place.formatted_address +
+            "<br>" + "<a style='color:blue;' target='_blank' href='"+ place.url + "'>" + place.url +"</a>" +
+            "<br>" + "Tel: " + place.formatted_phone_number + "</div>"
+        var imageTemplate = "<div class='infoWin2' style='width:550px; height:250px;'><strong style='font-size: 20px;'>" + place.name + "</strong>" +
+            "<br>" + place.formatted_address + '<br>'+ '<div class="infoImage"><img src =' +  imageUrls[0] +' >' +'<img src =' + imageUrls[1] +' >' + '<img src =' + imageUrls[2] +' ></div>' +
+            "<br>" + "<a style='color:blue;' target='_blank' href='"+ place.url + "'>" + place.url +"</a>" +
+            "<br>" + "Tel: " + place.formatted_phone_number + "</div>"
+
+        google.maps.event.addListener(marker, 'click', function() {
+          if(imageUrls.length === 0){
+            infowindow.setContent(template);
+          } else {
+            infowindow.setContent(imageTemplate);
+          }
+          infowindow.open(map, this);
+        });
+      })
+
+    })
+
+      // calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      // function calculateAndDisplayRoute(directionsDisplay, directionsService,
+      //     markerArray, stepDisplay, map) {
+      //   // First, remove any existing markers from the map.
+      //   for (var i = 0; i < markerArray.length; i++) {
+      //     markerArray[i].setMap(null);
+      //   }
+
+        // Retrieve the start and end locations and create a DirectionsRequest using
+        // WALKING directions.
+      //   directionsService.route(request, function(response, status) {
+      //     // Route the directions and pass the response to a function to create
+      //     // markers for each step.
+      //     if (status === 'OK') {
+      //       // document.getElementById('warnings-panel').innerHTML =
+      //       //     '<b>' + response.routes[0].warnings + '</b>';
+      //       directionsDisplay.setDirections(response);
+      //       showSteps(response, markerArray, stepDisplay, map);
+      //     } else {
+      //       window.alert('Directions request failed due to ' + status);
+      //     }
+      //   });
+      // }
+
+
+      // function showSteps(directionResult, markerArray, stepDisplay, map) {
+      //   // For each step, place a marker, and add the text to the marker's infowindow.
+      //   // Also attach the marker to an array so we can keep track of it and remove it
+      //   // when calculating new routes.
+      //   var myRoute = directionResult.routes[0].legs[0];
+      //   for (var i = 0; i < myRoute.steps.length; i++) {
+      //     var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+      //     marker.setMap(map);
+      //     marker.setPosition(myRoute.steps[i].start_location);
+      //     attachInstructionText(
+      //         stepDisplay, marker, myRoute.steps[i].instructions, map);
+      //   }
+      // }
+
+      // function attachInstructionText(stepDisplay, marker, text, map) {
+      //   google.maps.event.addListener(marker, 'click', function() {
+      //     // Open an info window when the marker is clicked on, containing the text
+      //     // of the step.
+      //     stepDisplay.setContent(text);
+      //     stepDisplay.open(map, marker);
+      //   });
+      // }
+
+
+
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(response);
       }
     });
   };
+
 
   // ERROR HANDLER
 
